c0c93da48472ad6f973b2b2909f3cb9c259817a6
Edwin Augustine <auztintech@gmail.com>
2023-07-31 17:08:28 +0100

    Create a temporary table to calculate the total number of non-unique fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,13 +1,5 @@
--- Create a temporary table to calculate the total number of non-unique fans for each country
-CREATE TEMPORARY TABLE temp_country_fans AS
-SELECT origin, SUM(nb_fans) AS total_fans
-FROM metal_bands
-GROUP BY origin;
-
--- Rank the countries based on the total number of fans in descending order
-SELECT origin, total_fans
-FROM temp_country_fans
-ORDER BY total_fans DESC;
-
--- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS temp_country_fans;
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+SELECT origin, SUM(fans) AS nb_fans
+    FROM metal_bands
+    GROUP BY origin
+    ORDER BY nb_fans DESC;
